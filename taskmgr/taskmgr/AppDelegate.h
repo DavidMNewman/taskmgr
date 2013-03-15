9c546218ace65c01999c4a5b6ba3ad41f0fcc22c
Steven Guitar <stguitar@gmail.com>
2013-03-15 01:14:57 -0400

    initial commit

---
+++ taskmgr/taskmgr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  taskmgr
+//
+//  Created by Steven Guitar on 3/15/13.
+//  Copyright (c) 2013 OBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ taskmgr/taskmgr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  taskmgr
+//
+//  Created by Steven Guitar on 3/15/13.
+//  Copyright (c) 2013 OBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
