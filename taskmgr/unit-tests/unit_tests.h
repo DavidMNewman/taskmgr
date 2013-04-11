23a7dfe65969891f16b365bf2f8a6b5a41920356
David Newman <davidnewman1986@gmail.com>
2013-04-11 09:36:22 -0400

    Editted scheme to allow Teamcity integration for testing purposes.

---
+++ taskmgr/unit-tests/unit_tests.h
@@ -0,0 +1,13 @@
+//
+//  unit_tests.h
+//  unit-tests
+//
+//  Created by David Newman on 4/5/13.
+//  Copyright (c) 2013 OBS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface unit_tests : SenTestCase
+
+@end
---
+++ taskmgr/unit-tests/unit_tests.m
@@ -0,0 +1,32 @@
+//
+//  unit_tests.m
+//  unit-tests
+//
+//  Created by David Newman on 4/5/13.
+//  Copyright (c) 2013 OBS. All rights reserved.
+//
+
+#import "unit_tests.h"
+
+@implementation unit_tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in unit-tests");
+}
+
+@end
